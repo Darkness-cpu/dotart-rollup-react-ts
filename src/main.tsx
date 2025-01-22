dfadfc489c6ec03274854bb8a175d03616293230
Darkness-cpu <mistakessadly@gmail.com>
2025-01-22 19:57:33 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./app";
 const root = document.getElementById("root");
 if (root) {
   createRoot(root).render(
     <React.StrictMode>
-      <div>
-        <h2>Hello World</h2>
-      </div>
+      <App />
     </React.StrictMode>
   );
 } else {
