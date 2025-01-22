0e60fd48d877ff7a6f73d87baba598fdd1627999
Darkness-cpu <mistakessadly@gmail.com>
2025-01-23 03:39:10 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,9 +3,6 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-
-
-
 export default {
   input: "src/main.tsx",
   output: [
@@ -17,15 +14,13 @@ export default {
   plugins: [
     replace({
       preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify('production')
-    }),
+      'process.env.NODE_ENV': JSON.stringify('production') }),
     nodeResolve({ browser: true }),
     babel({
-      
+      babelHelpers: 'bundled',
       presets: ['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],
       exclude: 'node_modules',
-      extensions: ['.ts','.tsx'],
-    }),
+      extensions: ['.ts','.tsx'] }),
     commonjs(),
     typescript({ tsconfig: './tsconfig.json' })
   ]};
