181fe94bc64fdb880e6442cc7324a2e8b2a9b634
Darkness-cpu <mistakessadly@gmail.com>
2025-01-23 01:17:23 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
-const root = document.getElementById("root");
-if (root) {
-  createRoot(root).render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  console.log("Could not find a DOM element with ID 'root'.");
-}
+
+const root = createRoot(document.getElementById('root') as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
