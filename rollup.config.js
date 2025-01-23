dd8189583925fe1997714bc4e47430b591185048
Darkness-cpu <mistakessadly@gmail.com>
2025-01-23 13:47:27 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+const ReactCompilerConfig = {
+  target: '18' // '17' | '18' | '19'
+};
 export default {
   input: "src/main.tsx",
   output: [
@@ -29,6 +32,9 @@ export default {
         '@babel/preset-env',
         '@babel/preset-react',
         '@babel/preset-typescript'
+      ],
+      plugins:[
+        'babel-plugin-react-compiler': ReactCompilerConfig
       ],
       exclude:'node_modules/**',
       extensions: ['.ts','.tsx']
