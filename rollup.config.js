98d64d28919f8e7b3a2082b9012eaaefc1050ad2
Darkness-cpu <mistakessadly@gmail.com>
2025-01-24 01:08:35 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,23 +5,11 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 export default {
   input: "src/main.tsx",
-  output: [
-    {
-      file: "dist/bundler.js",
-      format: "umd"
-    },
-  ],
+  output: [{file:"dist/bundler.js",format: "umd"}],
   plugins: [
-    replace({
-      preventAssignment: true,
-      'process.env.NODE_ENV': JSON.stringify('production') }),
+    replace({preventAssignment:true,'process.env.NODE_ENV': JSON.stringify('production') }),
     nodeResolve({browser:true}),
-    babel({
-      babelHelpers: 'bundled',
-      presets: ['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],
-      plugins:[ ['babel-plugin-react-compiler', { target: '18' }] ],
-      exclude: 'node_modules',
-      extensions: ['.ts','.tsx'] }),
+    babel({babelHelpers:'bundled',presets:['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],plugins:[ ['babel-plugin-react-compiler',{target:'18'}]],exclude: 'node_modules/**',extensions:['.ts','.tsx']}),
     commonjs(),
     typescript({tsconfig:'./tsconfig.json',outputToFilesystem:true,})
   ]};
