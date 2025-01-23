51504fa5a6828408267fad28ab888564f3553fda
Darkness-cpu <mistakessadly@gmail.com>
2025-01-23 10:48:49 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,9 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+const ReactCompilerConfig = {
+  target: '18' // '17' | '18' | '19'
+};
 export default {
   input: "src/main.tsx",
   output: [
@@ -24,7 +27,8 @@ export default {
       babelHelpers: 'bundled',
       presets: ['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],
       exclude: 'node_modules',
-      extensions: ['.ts','.tsx'] }),
+      extensions: ['.ts','.tsx'],
+      plugins: ['babel-plugin-react-compiler', ReactCompilerConfig] },
     commonjs(),
     typescript({ tsconfig: './tsconfig.json' })
   ]};
