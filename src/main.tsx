ef0c7d0567dd3fbd0815a00ec0457df62d68c876
Darkness-cpu <mistakessadly@gmail.com>
2025-01-24 17:35:23 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./app";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+if (root) {
 root.render(
   <React.StrictMode>
       <App />
