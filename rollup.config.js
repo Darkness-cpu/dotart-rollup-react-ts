ac477ae7e32c498b00aa8df81bfe8e6c3e1081a5
Darkness-cpu <mistakessadly@gmail.com>
2025-01-26 17:17:41 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default {
   plugins: [
     replace({preventAssignment:true,'process.env.NODE_ENV': JSON.stringify('production') }),
     nodeResolve({browser:true}),
-    babel({babelHelpers:'bundled',presets:['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],plugins:[ ['babel-plugin-react-compiler',{target:'18'}]],exclude: 'node_modules/**',extensions:['.ts','.jsx']}),
+    babel({babelHelpers:'bundled',presets:['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],plugins:[ ['babel-plugin-react-compiler',{target:'18'}]],exclude: './node_modules',extensions:['.ts','.tsx','js','jsx']}),
     commonjs(),
     typescript({tsconfig:'./tsconfig.json',outputToFilesystem:true,}),
     filesize()
