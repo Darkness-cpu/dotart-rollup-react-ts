18cb8fe66b20cf7ea43275a5ccd85a875f68047e
Darkness-cpu <mistakessadly@gmail.com>
2025-01-27 00:08:40 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import VitalsDisplay from './src/VitalsDisplay';
 import App from "./app";
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 if (root) {
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(
+<React.StrictMode>
+  <App />
+  <VitalsDisplay />
+</React.StrictMode>
+);
 
 } else { console.error("Root element not found!"); }
