dd1ed94e3d1a0e2a981597eae93ca0feec686069
Darkness-cpu <mistakessadly@gmail.com>
2025-01-26 23:17:08 +0700

    Update rollup.config.ts

--- rollup.config.ts
+++ rollup.config.ts
@@ -4,9 +4,11 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json' with { type: 'json' };
 const config: RollupOptions {
   input: "src/main.tsx",
   output: [{file:"dist/bundler.js",format:"iife"}],
+  external: Object.keys(pkg.dependencies),
   plugins: [
     replace({preventAssignment:true,'process.env.NODE_ENV': JSON.stringify('production') }),
     nodeResolve({browser:true}),
