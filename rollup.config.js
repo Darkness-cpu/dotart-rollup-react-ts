72e20bf7b6956aa3bfb405984148d2e14149fa9a
Darkness-cpu <mistakessadly@gmail.com>
2025-01-26 17:21:12 +0700

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,15 +3,14 @@ import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-import filesize from 'rollup-plugin-filesize';
 export default {
   input: "src/main.tsx",
-  output: [{file:"dist/bundler.js",format: "umd"}],
+  output: [{file:"dist/bundler.js",format: "iife"}],
   plugins: [
     replace({preventAssignment:true,'process.env.NODE_ENV': JSON.stringify('production') }),
     nodeResolve({browser:true}),
     babel({babelHelpers:'bundled',presets:['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],plugins:[ ['babel-plugin-react-compiler',{target:'18'}]],exclude: './node_modules',extensions:['.ts','.tsx','js','jsx']}),
     commonjs(),
-    typescript({tsconfig:'./tsconfig.json',outputToFilesystem:true,}),
-    filesize()
-  ]};
+    typescript({tsconfig:'./tsconfig.json',outputToFilesystem:true,})
+  ]
+};
