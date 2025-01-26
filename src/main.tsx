c62267712fc8564b338d1c6bfc55a60b884fa269
Darkness-cpu <mistakessadly@gmail.com>
2025-01-27 00:13:38 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import VitalsDisplay from './src/VitalsDisplay';
 import App from "./app";
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 if (root) {
 root.render(
 <React.StrictMode>
   <App />
-  <VitalsDisplay />
 </React.StrictMode>
 );
 
