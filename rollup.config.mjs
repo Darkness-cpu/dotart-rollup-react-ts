b5c590c726ba483755a387a40601249e6b4a5187
Darkness-cpu <mistakessadly@gmail.com>
2025-01-26 23:21:30 +0700

    Update and rename rollup.config.ts to rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,11 +1,11 @@
-import type { RollupOptions } from 'rollup';
 import replace from '@rollup/plugin-replace';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json' with { type: 'json' };
-const config: RollupOptions {
+import tsc from './tsconfig.json' with { type: 'json' };
+export default {
   input: "src/main.tsx",
   output: [{file:"dist/bundler.js",format:"iife"}],
   external: Object.keys(pkg.dependencies),
@@ -14,8 +14,6 @@ const config: RollupOptions {
     nodeResolve({browser:true}),
     babel({babelHelpers:'bundled',presets:['@babel/preset-env','@babel/preset-react','@babel/preset-typescript'],plugins:[ ['babel-plugin-react-compiler',{target:'18'}]],exclude: './node_modules',extensions:['.ts','.tsx']}),
     commonjs(),
-    typescript({tsconfig:'./tsconfig.json',outputToFilesystem:true,})
+    typescript({tsconfig: tsc,outputToFilesystem:true,})
   ]
 };
-
-export default config
